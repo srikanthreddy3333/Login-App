9ea9b0f74b2dab3cf0319b97c8353cb93fad310a
srikanth <srikanthreddy3501@gmail.com>
2023-04-27 06:09:39 +0000

    login app

---
+++ src/components/Logout/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+const Logout = props => {
+  const {logout} = props
+  return (
+    <button type="button" className="button" onClick={logout}>
+      Logout
+    </button>
+  )
+}
+export default Logout
